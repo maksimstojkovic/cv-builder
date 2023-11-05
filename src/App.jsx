8c87da5ccfb08115f655a385bea526a35e3d5459
Maksim Stojkovic <18454392+maksimstojkovic@users.noreply.github.com>
2023-11-05 22:22:24 +1100

    Clear boilerplate and add editable field

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+function Navbar() {
+  const sections = ["General", "Education", "Experience"];
+
+  return (
+    <div className="navbar">
+      {sections.map((section) => {
+        return (
+          <div key={section} className="navbar-item">
+            {section}
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+function EditableInput() {
+  const [editMode, setEditMode] = useState(false);
+  const [value, setValue] = useState("Blank");
+
+  if (editMode) {
+    return (
+      <input
+        type="text"
+        value={value}
+        onChange={(event) => setValue(event.target.value)}
+        onKeyUp={(event) => {
+          if (event.key === "Enter") setEditMode(false);
+        }}
+      />
+    );
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div
+        className="field"
+        onClick={() => {
+          setEditMode(true);
+        }}
+      >
+        {value}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+function App() {
+  return (
+    <>
+      <Navbar />
+      <EditableInput />
+    </>
+  );
+}
+
+export default App;
