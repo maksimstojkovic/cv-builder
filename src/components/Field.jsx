65e940f053e4ee2a31d144bc5a7330a6fd0c64e5
Maksim Stojkovic <18454392+maksimstojkovic@users.noreply.github.com>
2023-11-06 00:39:48 +1100

    Create basic field using editable input and label

--- src/components/Field.jsx
+++ src/components/Field.jsx
@@ -1,1 +1,13 @@
-import "../styles/Input.scss";
+import "../styles/Field.scss";
+import EditableInput from "./EditableInput";
+
+function Field({ id, label, initialValue }) {
+  return (
+    <div className="field">
+      <label htmlFor={id}>{label}</label>
+      <EditableInput initialValue={initialValue} />
+    </div>
+  );
+}
+
+export default Field;
