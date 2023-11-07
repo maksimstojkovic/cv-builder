5b54f7945839b24a8bde261817fd0602479cb44f
Maksim Stojkovic <18454392+maksimstojkovic@users.noreply.github.com>
2023-11-08 00:02:40 +1100

    Add base fields for general section

--- src/components/General.jsx
+++ src/components/General.jsx
@@ -4,7 +4,11 @@ import Field from "./Field";
 function General() {
   return (
     <div className="general">
-      <Field id="name" label="Name" initialValue="" />
+      <Field id="name" label="Name" />
+      <Field id="location" label="Location" />
+      <Field id="email" label="Email" />
+      <Field id="phone" label="Phone" />
+      <Field id="about" label="About" isTextArea="false" />
     </div>
   );
 }
