b1e1c00d30fb773c4b907094c5bedcfa064bf19b
Maksim Stojkovic <18454392+maksimstojkovic@users.noreply.github.com>
2023-11-09 01:45:32 +1100

    Assemble app

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,53 @@
-import Navbar from "./components/Navbar";
 import General from "./components/General";
 import Experience from "./components/Experience";
 import Education from "./components/Education";
+import Activities from "./components/Activities";
+import Skills from "./components/Skills";
+
+import { flushSync } from "react-dom";
 
 function App() {
   return (
-    <div className="wrapper">
-      {/* <Navbar /> */}
-      <General />
-      <Experience />
-      <Education />
-    </div>
+    <>
+      <header>
+        <h2>CV Builder</h2>
+        <div className="controls">
+          <button
+            type="button"
+            onClick={() => {
+              console.log("print");
+
+              console.log(document.querySelectorAll("button.save"));
+
+              // Force react to render DOM
+              flushSync(() => {
+                document
+                  .querySelectorAll("button.save")
+                  .forEach((button) => button.click());
+              });
+
+              window.print();
+            }}
+          >
+            Print
+          </button>
+        </div>
+      </header>
+
+      <div className="content">
+        <div className="wrapper">
+          <General />
+          <Experience />
+          <Education />
+          <Activities />
+          <Skills />
+        </div>
+      </div>
+
+      <footer>
+        &copy; Copyright {new Date().getFullYear()}, Maksim Stojkovic
+      </footer>
+    </>
   );
 }
 
